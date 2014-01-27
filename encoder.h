03558ea498e1edd236df13d79e0b5b5a4ecd27fc
Matt Keeter <matt.j.keeter@gmail.com>
2014-01-27 18:24:33 -0500

    Changed pin in encoder_switch macro

--- encoder.h
+++ encoder.h
@@ -1,11 +1,13 @@
 #ifndef ENCODER_H
 #define ENCODER_H
 
+#include <avr/io.h>
+
 // The encoder is defined by two values:
 //      encoder is an int representing ticks in either direction
-//      encoder_switch is a macro to evaluates to true or false
+//      encoder_switch is a macro to check the desired pin
 extern volatile int encoder;
-#define encoder_switch bool(!(PIND & (1 << PD0)))
+#define encoder_switch (!(PINB & (1 << PB6)))
 
 void encoder_init();
 
