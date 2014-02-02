432f9cb4aa26b7c6fdedcc1a3f81e060175755b1
Matt Keeter <matt.j.keeter@gmail.com>
2014-02-02 12:53:07 -0500

    Make LEDs variable brightness

--- leds.c
+++ leds.c
@@ -1,7 +1,14 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 #include "leds.h"
 #include "macros.h"
+
+uint8_t brightness[8];      // Values from 0 to 7, with 7 being brightest
+volatile uint8_t tick = 0;  // Goes from 0 to 7.
+uint8_t levels[8] = {
+    0, 10, 20, 35, 55, 80, 110, 145
+};
 
 void LEDs_init()
 {
@@ -11,14 +18,23 @@ void LEDs_init()
     OUTPUT(DDRB, PB4);  // DS (serial data input)
 
     CLEAR(PORTD, PD7);   // Set output enable to 0 (enables outputs)
-    LEDs(0);
+
+    SET(TIMSK0, OCIE0A); // output compare interrupt
+    OCR0A = levels[0];  // output compare register
+
+    SET(TCCR0B, CS02); // 256x prescalar
+
+    for (int i=0; i < 8; ++i)
+    {
+        brightness[i] = (i > 6) ? 6 : i;
+    }
 }
 
-void LEDs(const char L)
+void update_LEDs()
 {
-    for (uint8_t i=1; i != 0; i <<= 1) {
-        // Set the data going into the shift register (DS)
-        if (L & i) {
+    for (int i=0; i < 8; ++i)
+    {
+        if (brightness[i] > tick) {
             SET(PORTB, PB4);
         } else {
             CLEAR(PORTB, PB4);
@@ -32,4 +48,13 @@ void LEDs(const char L)
     // Toggle STCP high and low
     SET(PORTD, PD6);
     CLEAR(PORTD, PD6);
+
+}
+
+
+ISR(TIMER0_COMPA_vect)
+{
+    update_LEDs();
+    tick = (tick + 1) % 8;
+    OCR0A = levels[tick];
 }
