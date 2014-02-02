068c9343cd3b17f5f9d49549ee9d1a6591cf8d3b
Matt Keeter <matt.j.keeter@gmail.com>
2014-02-02 17:32:26 -0500

    Adding timer that counts tenths of a second

---
+++ tenths.c
@@ -0,0 +1,25 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "tenths.h"
+#include "macros.h"
+
+volatile unsigned tenths = 0;
+
+void tenths_init()
+{
+    // Hit output compare about once every tenth of a second
+    OCR1AH = 0x07;
+    OCR1AL = 0x35;
+
+    SET(TCCR1B, CS12); // 256x prescalar
+    SET(TIMSK1, OCIE1A);    // trigger interrupt
+}
+
+// After each tenth of a second, reset the timer.
+ISR(TIMER1_COMPA_vect)
+{
+    tenths++;
+    TCNT1H = 0;
+    TCNT1L = 0;
+}
---
+++ tenths.h
@@ -0,0 +1,7 @@
+#ifndef MILLIS_H
+#define MILLIS_H
+
+void tenths_init();
+extern volatile unsigned tenths;
+
+#endif
