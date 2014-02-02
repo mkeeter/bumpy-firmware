b90e18987837013a97989bea3686a77fd1582990
Matt Keeter <matt.j.keeter@gmail.com>
2014-02-01 20:12:47 -0500

    SD function to skip to next song

--- sd.c
+++ sd.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+
+#include "sd.h"
 
 #include "sd-reader/sd_raw.h"
 #include "sd-reader/fat.h"
 #include "sd-reader/partition.h"
 
-struct partition_struct* partition;
-struct fat_fs_struct* fs;
+struct partition_struct* partition = NULL;
+struct fat_fs_struct* fs = NULL;
+struct fat_dir_struct* root = NULL;
+
+struct fat_dir_entry_struct file_dir;
+struct fat_file_struct* file = NULL;
 
 int sd_init()
 {
@@ -29,5 +36,47 @@ int sd_init()
         return 0;
     }
 
+    struct fat_dir_entry_struct root_dir;
+    fat_get_dir_entry_of_path(fs, "/", &root_dir);
+    root = fat_open_dir(fs, &root_dir);
+
+    sd_next_song();
+
     return 1;
+}
+
+void sd_next_song()
+{
+    // Flag to prevent infinite loop if we don't have any .mp3 files
+    int reset_already = 0;
+
+    while (1)
+    {
+        // If we've reached the last file, reset the directory
+        if (!fat_read_dir(root, &file_dir))
+        {
+            fat_reset_dir(root);
+
+            // Don't loop forever if we've got no .mp3 files
+            if (reset_already)  return;
+
+            // Set this flag so that if we get here again we abort.
+            reset_already = 1;
+        }
+
+        // Check to see if this directory entry has mp3 as its extension
+        else if (!strcmp(
+                    "mp3", file_dir.long_name +
+                           strlen(file_dir.long_name) - 3))
+        {
+            break;
+        }
+    }
+
+    file = fat_open_file(fs, &file_dir);
+}
+
+void sd_get_data(uint8_t* buffer, uintptr_t amount)
+{
+    if (file)   fat_read_file(file, buffer, amount);
 }
--- sd.h
+++ sd.h
@@ -1,8 +1,13 @@
 #ifndef SD_H
 #define SD_H
 
+#include <stdint.h>
+
 // Initializes the SD filesystem.
 // Returns 1 if success, 0 otherwise.
 int sd_init();
+void sd_next_song();
+
+void sd_get_data(uint8_t* buffer, uintptr_t amount);
 
 #endif
